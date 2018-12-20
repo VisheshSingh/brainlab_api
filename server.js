f42faf0b543abfd991c3f26d466e0aefe1988762
Vishesh <visheshsingh16@gmail.com>
2018-12-19 23:09:54 -0500

    Signin and register routes

--- server.js
+++ server.js
@@ -1,9 +1,55 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
-
+app.use(bodyParser.json());
+const database = {
+  users: [
+    {
+      id: 123,
+      name: "Todd",
+      email: "Todd@gmail.com",
+      password: "cookies",
+      entries: 0,
+      joined: new Date()
+    },
+    {
+      id: 124,
+      name: "Sally",
+      email: "Sally@gmail.com",
+      password: "bananas",
+      entries: 0,
+      joined: new Date()
+    }
+  ]
+};
 app.get("/", (req, res) => {
-  res.send("It works!");
+  res.send(database.users);
+});
+
+app.post("/signin", (req, res) => {
+  const { email, password } = req.body;
+  if (
+    email === database.users[0].email &&
+    password === database.users[0].password
+  ) {
+    res.json("success");
+  } else {
+    res.status(400).json("Error signing in");
+  }
+});
+
+app.post("/register", (req, res) => {
+  const { name, email, password } = req.body;
+  database.users.push({
+    id: 125,
+    name: name,
+    email: email,
+    password: password,
+    entries: 0,
+    joined: new Date()
+  });
+  res.json(database.users[database.users.length - 1]);
 });
 
 app.listen(3000, () => {
